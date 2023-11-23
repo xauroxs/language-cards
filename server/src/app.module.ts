22205763213be68e2a144a0057b144b63d60d7d4
xauroxs <xauroxs@gmail.com>
2023-11-23 11:50:26 +0300

    add connection to db, setup config service

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,36 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { configValidationSchema } from './config.schema';
 
 import { LanguagesModule } from './languages/languages.module';
-import { LanguagesService } from './languages/languages.service';
 
 @Module({
-  imports: [LanguagesModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      validationSchema: configValidationSchema,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
+    }),
+    LanguagesModule,
+  ],
   controllers: [],
-  providers: [LanguagesService],
+  providers: [],
 })
 export class AppModule {}
--- server/src/languages/languages.module.ts
+++ server/src/languages/languages.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { LanguagesService } from './languages.service';
 import { LanguagesController } from './languages.controller';
 
+import { Language } from './language.entity';
+
 @Module({
+  imports: [TypeOrmModule.forFeature([Language])],
   providers: [LanguagesService],
   controllers: [LanguagesController],
 })
