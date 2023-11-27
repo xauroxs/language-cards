42349d68d7e69250e2f9523671a0084d7ca62966
xauroxs <xauroxs@gmail.com>
2023-11-27 22:44:42 +0300

    apply dark mode styles to not found page

--- client/src/pages/not-found/not-found.page.tsx
+++ client/src/pages/not-found/not-found.page.tsx
@@ -6,14 +6,14 @@ const NotFoundPage = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
+    <div className="flex h-screen flex-col items-center justify-center bg-slate-100 dark:bg-black">
       <div className="h-72 w-72">
         <img src={Lost} alt="lost person" />
       </div>
-      <h2 className="mb-2 text-4xl font-bold">Not Found</h2>
-      <p className="mb-2 text-lg">Where am I?</p>
+      <h2 className="mb-2 text-4xl font-bold dark:text-slate-100">Not Found</h2>
+      <p className="mb-2 text-lg dark:text-slate-100">Where am I?</p>
       <button
-        className="flex h-12 items-center justify-center rounded-xl bg-sky-500 p-4 text-lg text-white hover:bg-sky-600"
+        className="flex h-12 items-center justify-center rounded-xl bg-sky-500 p-4 text-lg text-white transition hover:bg-sky-600"
         onClick={() => navigate("/")}
       >
         Teleport Home
