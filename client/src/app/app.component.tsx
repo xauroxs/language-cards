21d1760583490d6c67c50ec0a01e1fb3b4753f85
xauroxs <xauroxs@gmail.com>
2023-11-27 22:25:06 +0300

    add react router, create not-found page

--- client/src/app/app.component.tsx
+++ client/src/app/app.component.tsx
@@ -1,5 +1,28 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import NotFoundPage from "../pages/not-found/not-found.page";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/">
+        <Route
+          path="not-found"
+          element={<NotFoundPage />}
+        />
+
+        <Route
+          path="*"
+          element={
+            <Navigate
+              to="/not-found"
+              replace
+            />
+          }
+        />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
---
+++ client/src/pages/not-found/not-found.page.tsx
@@ -0,0 +1,28 @@
+import { useNavigate } from "react-router-dom";
+
+import Lost from "../../assets/lost.png";
+
+const NotFoundPage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <div className="h-72 w-72">
+        <img
+          src={Lost}
+          alt="lost person"
+        />
+      </div>
+      <h2 className="mb-2 text-4xl font-bold">Not Found</h2>
+      <p className="mb-2 text-lg">Where am I?</p>
+      <button
+        className="flex h-12 items-center justify-center rounded-xl bg-sky-500 p-4 text-lg text-white hover:bg-sky-600"
+        onClick={() => navigate("/")}
+      >
+        Teleport Home
+      </button>
+    </div>
+  );
+};
+
+export default NotFoundPage;
